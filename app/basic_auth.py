69a50e75f73935f2aba7e6c97031f6d05b20a25a
Jesus <jiesusdance@gmail.com>
2026-06-16 15:48:21 +0300

    added Basic Auth

---
+++ app/basic_auth.py
@@ -0,0 +1,25 @@
+import secrets
+from typing import Annotated
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBasicCredentials, HTTPBasic
+
+security = HTTPBasic()
+BASIC_AUTH = Annotated[HTTPBasicCredentials, Depends(security)]
+
+def get_current_username(credentials: BASIC_AUTH):
+    current_username_b = credentials.username.encode('utf-8')
+    correct_username_b = b'admin'
+    is_correct_username = secrets.compare_digest(current_username_b, correct_username_b)
+
+    current_password_b = credentials.password.encode('utf-8')
+    correct_password_b = b'admin_pass'
+    is_correct_password = secrets.compare_digest(current_password_b, correct_password_b)
+
+    if not (is_correct_username and is_correct_password):
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Basic"}
+        )
+    return credentials.username
