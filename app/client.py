f9c502be1d19131061dd299ba75e3a3d239e6188
Jesus <jiesusdance@gmail.com>
2026-06-16 15:45:03 +0300

    added logging for url status, refactor BookClient with DI

--- app/client.py
+++ app/client.py
@@ -1,29 +1,36 @@
 import httpx
 import logging
 from fastapi import HTTPException
+from starlette.requests import Request
 
-# logging.basicConfig(
-#     format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
-#     datefmt="%Y-%m-%d %H:%M:%S",
-#     level=logging.DEBUG
-# )
-# httpx.get("")
+logger = logging.getLogger(__name__)
+
+logging.basicConfig(
+    format="%(levelname)s [%(asctime)s] %(name)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    level=logging.INFO
+)
+httpx.get("https://openlibrary.org")
+
+
+def get_httpx_client(request: Request) -> httpx.AsyncClient:
+    return request.app.state.httpx_client
 
 
 class BookClient:
-    def __init__(self, url: str):
+    def __init__(self, url: str, client: httpx.AsyncClient):
         self.url = url
+        self.client = client
 
     async def fetch_book_from_api(self, title: str):
         try:
-            async with httpx.AsyncClient(http2=True) as client:
-                response = await client.get(self.url + title, timeout=10)
-                response.raise_for_status()
-                docs = response.json().get("docs")
-                title, author = (
-                    docs[0].get("title", "Unknown"),
-                    docs[0].get("author_name", "Unknown")[0],
-                )
+            response = await self.client.get(self.url + title, timeout=10)
+            response.raise_for_status()
+            docs = response.json().get("docs", [])
+            title, author = (
+                docs[0].get("title", "Unknown"),
+                docs[0].get("author_name", "Unknown")[0],
+            )
             if not title or not author:
                 raise HTTPException(404, "Invalid response from API")
             return title, author
@@ -31,3 +38,5 @@ class BookClient:
             raise HTTPException(504, "External API timeout")
         except httpx.RequestError:
             raise HTTPException(404, "Book not found in external API")
+        except httpx.ConnectError:
+            raise HTTPException(429, "Max size of pools")
