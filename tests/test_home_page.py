69256275263a1ab0e3be3460f44235a3002a0c68
Jesus <jiesusdance@gmail.com>
2026-06-18 14:32:12 +0300

    fixed test_home_page for basic auth

--- tests/test_home_page.py
+++ tests/test_home_page.py
@@ -2,10 +2,10 @@ import pytest
 
 
 @pytest.mark.asyncio
-async def test_home_page(test_client):
-    response = await test_client.get("/")
+async def test_home_page(test_client_api):
+    response = await test_client_api.get("/", auth=("admin", "admin_pass"))
     json_data = response.json()
 
     assert response.status_code == 200
     assert "message" in json_data
-    assert json_data["message"] == "Hello from FastAPI!"
+    assert json_data["message"] == "Hello admin"
