2e5861c0dbbec038366a51d167f8f9fcc39c6655
Jesus <jiesusdance@gmail.com>
2026-06-29 19:47:05 +0300

    added env variables to username and password

--- app/basic_auth.py
+++ app/basic_auth.py
@@ -4,22 +4,25 @@ from typing import Annotated
 from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBasicCredentials, HTTPBasic
 
+from app.config import settings
+
 security = HTTPBasic()
 BASIC_AUTH = Annotated[HTTPBasicCredentials, Depends(security)]
 
+
 def get_current_username(credentials: BASIC_AUTH):
-    current_username_b = credentials.username.encode('utf-8')
-    correct_username_b = b'admin'
+    current_username_b = credentials.username.encode("utf-8")
+    correct_username_b = settings.ADMIN_NAME.encode("utf-8")
     is_correct_username = secrets.compare_digest(current_username_b, correct_username_b)
 
-    current_password_b = credentials.password.encode('utf-8')
-    correct_password_b = b'admin_pass'
+    current_password_b = credentials.password.encode("utf-8")
+    correct_password_b = settings.ADMIN_PASS.encode("utf-8")
     is_correct_password = secrets.compare_digest(current_password_b, correct_password_b)
 
     if not (is_correct_username and is_correct_password):
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
-            headers={"WWW-Authenticate": "Basic"}
+            headers={"WWW-Authenticate": "Basic"},
         )
     return credentials.username
