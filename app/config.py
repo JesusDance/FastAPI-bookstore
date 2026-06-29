44dd37a5912cd7d32816357b89c8d9f84678c68e
Jesus <jiesusdance@gmail.com>
2026-06-29 19:42:35 +0300

    fixed config, added more variables

--- app/config.py
+++ app/config.py
@@ -8,10 +8,14 @@ class Settings(BaseSettings):
     JWT_SECRET_KEY: str 
     ALGORITHM: str 
     REDIS_URL: str
+    ADMIN_NAME: str
+    ADMIN_PASS: str
 
     ORIGINS: list[str] = ["http://127.0.0.1:3000", "http://localhost:8080"]
     CACHE_TTL_SECONDS: int = 3600
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60
+    CACHE_EX_SECONDS: int = 60
+    LIMIT_OF_REQUESTS: int = 5
 
 
 settings = Settings()
