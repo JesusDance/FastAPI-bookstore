c549334361ae00a4f98ae8199f7db2e2c5eb26e0
Jesus <jiesusdance@gmail.com>
2026-07-01 18:08:42 +0300

    switching to sqlalchemy session

--- app/db.py
+++ app/db.py
@@ -1,18 +1,17 @@
 from typing import Annotated
 
 from fastapi import Depends
-from sqlmodel import Session, create_engine
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, Session
 
 from app.config import settings
 
-# sqlite_url = "sqlite:///db.sqlite"
-# engine = create_engine(sqlite_url, connect_args={"check_same_thread": False})
-
 engine = create_engine(settings.DATABASE_URL)
+session_local = sessionmaker(bind=engine)
 
 
 def get_session():
-    with Session(engine) as session:
+    with session_local() as session:
         yield session
 
 
